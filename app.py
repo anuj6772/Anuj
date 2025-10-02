448119aed6e3ac1807525c34d40ff8c7602d5e66
anuj6772 <studytimechauhan@gmail.com>
2025-10-02 15:44:57 +0000

    done

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import random
 import os
 
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/get_result", methods=["GET"])
 def get_result():
     # Weighted probabilities
-        colors = ["red", "green", "violet"]
-            weights = [0.45, 0.45, 0.10]  # red=45%, green=45%, violet=10%
+    colors = ["red", "green", "violet"]
+    weights = [0.45, 0.45, 0.10]  # red=45%, green=45%, violet=10%
+    result = random.choices(colors, weights=weights, k=1)[0]
+    return jsonify({"result": result})
 
-                result = random.choices(colors, weights=weights, k=1)[0]
-                    return jsonify({"result": result})
-
-                    if __name__ == "__main__":
-                        port = int(os.environ.get("PORT", 5000))  # Railway will assign PORT
-                            app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Railway will assign PORT
+    app.run(host="0.0.0.0", port=port)
 
 
 
